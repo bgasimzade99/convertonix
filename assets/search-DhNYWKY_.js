7da7321d1b6531af8bb55c788a47fd3d3030fd14
Coder999 <110833230+bgasimzade99@users.noreply.github.com>
2025-10-08 14:15:08 +0400

    Fix: Move build files to root for GitHub Pages deployment

---
+++ assets/search-DhNYWKY_.js
@@ -0,0 +1,11 @@
+import{c as e}from"./index-Djuotrda.js";/**
+ * @license lucide-react v0.294.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=e("BookOpen",[["path",{d:"M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z",key:"vv98re"}],["path",{d:"M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z",key:"1cyq3y"}]]);/**
+ * @license lucide-react v0.294.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=e("Search",[["circle",{cx:"11",cy:"11",r:"8",key:"4ej97u"}],["path",{d:"m21 21-4.3-4.3",key:"1qie3q"}]]);export{c as B,o as S};
